d88fe2b04156c663db4216a9d9fe8f97f14802d0
martin0230206 <martin0230206@gmail.com>
2024-12-23 21:49:39 +0800

    feat: add initial implementation of WinForms controls and project structure

---
+++ MT.WinForms/Controls/TextBoxAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MT.WinForms
+{
+    public class TextBoxAdapter : TextBox, ISystemControl
+    {
+        public string GetColumnName() => Tag == null ? "" : Tag.ToString();
+
+        public string GetValue() => this.Text;
+    }
+}
---
+++ MT.WinForms/SingleTableForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MT.WinForms
+{
+    public partial class SingleTableForm : Form
+    {
+        public SingleTableForm()
+        {
+            InitializeComponent();
+        }
+
+        protected virtual Dictionary<string, string> GetFormData()
+        {
+            var result = new Dictionary<string, string>();
+            foreach (ISystemControl control in this.Controls.OfType<ISystemControl>())
+            {
+                result.Add(control.GetColumnName(), control.GetValue());
+            }
+            return result;
+        }
+
+        protected virtual string GenerateInsertSql(string tableName)
+        {
+            var formData = GetFormData();
+            var columns = string.Join(", ", formData.Keys);
+            var values = "@" + string.Join("', @", formData.Values);
+
+            string sql = $"INSERT INTO {tableName} ({columns}) VALUES({values})";
+            return sql;
+        }
+    }
+}

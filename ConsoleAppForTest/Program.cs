a92f779e7592789dca60542cd1ebb1679598af6a
martin0230206 <martin0230206@gmail.com>
2025-05-14 16:41:35 +0800

    feat: 新增測試用專案

---
+++ ConsoleAppForTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using MT.Extensions;
+namespace ConsoleAppForTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var now = DateTime.Now;
+			var (s, e) = now.GetPreviousWeekRange();
+			Console.WriteLine($" {now} => {s} - {e}");
+		}
+	}
+}
